4149f940ca0920fc2893e92444669b227aa74ebd
Ilya Vasilyev <den11krs@gmail.com>
2025-05-16 10:41:57 +0400

    Do lesson 3.2 "Test Fixtures"

--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -1,15 +1,12 @@
-import { test, expect } from '@playwright/test';
-import { RequestHandler } from '../utils/request-handler';
+import { test } from '../utils/fixtures';
 
-test('first test', async({}) => {
-
-    const api = new RequestHandler();
+test('first test', async({ api }) => {
 
     api
         .url('https://conduit-api.bondaracademy.com/api')
         .path('/articles')
         .params({limit: 10, offset: 0})
         .headers({Authorization: 'athToken'})
-        .body({'user': { 'email': 'pwapiuser@test.com', 'password': 'Welcome' }})
+        .body({'user': { 'email': 'pwapiuser@test.com', 'password': 'Welcome' }});
 
-})
+});
